b844c09c54133615f74e12290f4964e3a95bbb62
vzxry <kennyanakku@gmail.com>
2024-06-27 14:56:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public'))); 
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html')); }); 
-
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'index.html')); }); 
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`); 
-});

7972633460e48ea76fcfc4ed3ba8f23709ae05ec
vzxry <kennyanakku@gmail.com>
2024-06-27 15:03:07 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public'))); 
 app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'index.html')); }); 
